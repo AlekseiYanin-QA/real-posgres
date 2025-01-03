9b14ff93ea80b6fe5dda5d444b05dd1cf0194024
Yanin Aleksey <aleksei.yanin.qa@gmail.com>
2025-01-03 15:55:27 +0300

    Initial commit

---
+++ src/main/java/com/roks/real_posgres/RealPosgresApplication.java
@@ -0,0 +1,28 @@
+package com.roks.real_posgres;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RealPosgresApplication {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealPosgresApplication.class, args);
+	}
+
+	@GetMapping("/repeat/{str}/{times}")
+	public String repeat(@PathVariable("str") String str, @PathVariable("times") int times) {
+		final String repeat = jdbcTemplate.queryForList("select repeat('"+str+"', "+times+")", String.class).get(0);
+		return String.format("Hello user, repeat of string is, according to DB: %s!", repeat);
+}
+}
---
+++ src/test/java/com/roks/real_posgres/RealPosgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roks.real_posgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealPosgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
